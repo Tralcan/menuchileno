0d4b088ef685a177a54a00bfd2fe75e0b14b1a08
Diego Anguita M. <tralcan@gmail.com>
2025-06-18 00:53:57 +0000

    Por favor agrandar más la ventana del iframe y quila la linea inferior q

--- src/components/menu/coffee-modal.tsx
+++ src/components/menu/coffee-modal.tsx
@@ -1,8 +1,7 @@
 
 "use client";
 
-import { Dialog, DialogContent, DialogFooter } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 interface CoffeeModalProps {
   isOpen: boolean;
@@ -14,18 +13,13 @@ export default function CoffeeModal({ isOpen, onClose }: CoffeeModalProps) {
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-lg md:max-w-xl lg:max-w-2xl max-h-[90vh] grid grid-rows-[1fr_auto] p-0 bg-background">
-        <div className="row-start-1 min-h-0 overflow-hidden flex flex-col">
-          <iframe
-            src={coffeeUrl}
-            title="Apóyame con un café en Buy Me A Coffee"
-            className="w-full h-full flex-grow border-0"
-            allowFullScreen
-          />
-        </div>
-        <DialogFooter className="p-4 border-t row-start-2">
-          <Button onClick={onClose} variant="outline">Cerrar</Button>
-        </DialogFooter>
+      <DialogContent className="w-[90vw] max-w-[1200px] h-[85vh] p-0 bg-background flex flex-col overflow-hidden">
+        <iframe
+          src={coffeeUrl}
+          title="Apóyame con un café en Buy Me A Coffee"
+          className="w-full h-full flex-grow border-0"
+          allowFullScreen
+        />
       </DialogContent>
     </Dialog>
   );
